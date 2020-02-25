9842a4d9727533432a769901e984cf1a552a62e2
war16641 <44914209+war16641@users.noreply.github.com>
2020-02-25 12:15:16 +0800

    Update table_to_structs.m

--- table_to_structs.m
+++ table_to_structs.m
@@ -5,7 +5,11 @@ vns=T.Properties.VariableNames;
 for i=1:size(T,1)
     t=struct();
     for i2=1:length(vns)
-        t.(vns{i2})=T{i,i2};
+        tp=T{i,i2};
+        if isa(tp,'cell') && length(tp)==1 %字符串会在table中以1*1 cell形式保存
+            tp=tp{1}; %去除cell套
+        end
+        t.(vns{i2})=tp;
     end
     structs=[structs t];
 end
